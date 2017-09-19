44f4f9735a33d8468bb753b612961411ea5ed003
erniu <qihea163@163.com>
2017-09-19 17:59:27 +0800

    mybatis自动生成

--- algo-git-boot/src/main/java/com/beyond/algo/algogitboot/AlgoGitBootApplication.java
+++ algo-git-boot/src/main/java/com/beyond/algo/algogitboot/AlgoGitBootApplication.java
@@ -1,11 +1,13 @@
 package com.beyond.algo.algogitboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.beyond.algo.controller","com.beyond.algo.infra","com.beyond.algo.infra.impl","com.beyond.algo.model"})
+@ComponentScan(basePackages={"com.beyond.algo.controller","com.beyond.algo.infra","com.beyond.algo.dao","com.beyond.algo.model"})
+@MapperScan(basePackages = "com.beyond.algo.dao.mapper")
 public class AlgoGitBootApplication {
 
 	public static void main(String[] args) {
