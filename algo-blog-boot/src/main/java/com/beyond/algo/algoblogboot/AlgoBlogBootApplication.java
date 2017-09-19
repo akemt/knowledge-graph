305abadb2939167bd07721cc4a49f01f4e43319b
erniu <qihea163@163.com>
2017-09-19 09:27:00 +0800

    初始化

---
+++ algo-blog-boot/src/main/java/com/beyond/algo/algoblogboot/AlgoBlogBootApplication.java
@@ -0,0 +1,12 @@
+package com.beyond.algo.algoblogboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoBlogBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoBlogBootApplication.class, args);
+	}
+}
---
+++ algo-git-boot/src/main/java/com/beyond/algo/algogitboot/AlgoGitBootApplication.java
@@ -0,0 +1,12 @@
+package com.beyond.algo.algo.algogitboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoGitBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoGitBootApplication.class, args);
+	}
+}
