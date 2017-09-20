b11a07a11462972cbdd4ac94b51f85d0d7a1cbd7
erniu <qihea163@163.com>
2017-09-20 10:43:49 +0800

    data

--- algo-git-boot/src/main/java/com/beyond/algo/model/GitConfigModel.java
+++ algo-git-boot/src/main/java/com/beyond/algo/model/GitConfigModel.java
@@ -1,54 +1,16 @@
 package com.beyond.algo.model;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix="gitlib")
+@Data
 public class GitConfigModel {
     private String baseUrl;
     private String rootName;
     private String rootPasswd;
     private String privateToken;
     private String localBasePath;
-
-    public String getBaseUrl() {
-        return baseUrl;
-    }
-
-    public void setBaseUrl(String baseUrl) {
-        this.baseUrl = baseUrl;
-    }
-
-    public String getRootName() {
-        return rootName;
-    }
-
-    public void setRootName(String rootName) {
-        this.rootName = rootName;
-    }
-
-    public String getRootPasswd() {
-        return rootPasswd;
-    }
-
-    public void setRootPasswd(String rootPasswd) {
-        this.rootPasswd = rootPasswd;
-    }
-
-    public String getPrivateToken() {
-        return privateToken;
-    }
-
-    public void setPrivateToken(String privateToken) {
-        this.privateToken = privateToken;
-    }
-
-    public String getLocalBasePath() {
-        return localBasePath;
-    }
-
-    public void setLocalBasePath(String localBasePath) {
-        this.localBasePath = localBasePath;
-    }
 }
