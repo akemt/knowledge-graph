0c5178eb2b9fa38c4909a422b2ed08d5dd67d5df
zhang1chang0 <1625488654@qq.com>
2017-09-22 10:26:34 +0800

    初步完成git项目ant编译打包删除功能

--- algo-git-boot/src/test/java/com/beyond/algo/algogitboot/impl/JGitServiceTest.java
+++ algo-git-boot/src/test/java/com/beyond/algo/algogitboot/impl/JGitServiceTest.java
@@ -15,53 +15,51 @@ import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
-
 @RunWith(SpringRunner.class)
-@RestClientTest({ GitLibService.class})
+@RestClientTest({GitLibService.class})
 public class JGitServiceTest {
     @Autowired
     private JGitService jGitService;
     @Autowired
     private BuildAntProjectService buildAntProjectService;
     private final static Logger logger = LoggerFactory.getLogger(JGitServiceTest.class);
-    GitUser gitUser=new GitUser();
-   @Test
+    GitUser gitUser = new GitUser();
+
+    @Test
     public void gitCloneProjectTest() throws Exception {
-        jGitService.gitCloneProject("http://192.168.1.88/test1/testproject.git","TestProject","test1","test1234");
+        jGitService.gitCloneProject("http://192.168.1.88/test1/testproject.git", "TestProject", "test1", "test1234");
     }
 
     @Test
-    public void initCommitAllFilesTest () throws Exception{
+    public void initCommitAllFilesTest() throws Exception {
 
-        jGitService.initCommitAndPushAllFiles("E:/repo/test1/TestProject/.git","test1","test1234");
+        jGitService.initCommitAndPushAllFiles("E:/repo/test1/TestProject/.git", "test1", "test1234");
     }
 
     @Test
     // 测试本地删除文件同时删除服务器
-    public void commitAndPushDelAllFiles () throws Exception{
-    jGitService.commitAndPushDelAllFiles("E:/repo/test1/TestProject/.git","test1","test1234","E:/repo/test1/TestProject/222.txt");
+    public void commitAndPushDelAllFiles() throws Exception {
+        jGitService.commitAndPushDelAllFiles("E:/repo/test1/TestProject/.git", "test1", "test1234", "E:/repo/test1/TestProject/222.txt");
     }
 
     @Test
     // 本地文件写入保存
-    public void  writeAndSaveFile () throws Exception{
-        String content="124444444443";
-        FileUntil.writeFile("E:/repo/test1/TestProject/23.txt",content);
+    public void writeAndSaveFile() throws Exception {
+        String content = "124444444443";
+        FileUntil.writeFile("E:/repo/test1/TestProject/23.txt", content);
 
     }
 
-   @Test
+    @Test
     // 本地编译打包上传测试
-    public void  BuildAntProject () throws Exception{
+    public void BuildAntProject() throws Exception {
         gitUser.setPassword("test1234");
         gitUser.setPath("E:/repo/test1/TestProject");
         gitUser.setDescDir("D:/test");
         gitUser.setUsername("test1");
-       buildAntProjectService.buildAndUpLoadProject(gitUser);
+        buildAntProjectService.buildAndUpLoadProject(gitUser);
 
     }
-
-
 
 
 }
