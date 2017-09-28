9b4565df54c2c3212a584e7257138c127ce0138a
zhang1chang0 <1625488654@qq.com>
2017-09-28 17:50:49 +0800

    增加共同方法

--- common/src/main/java/com/beyond/algo/common/PasswordEncrypUtil.java
+++ common/src/main/java/com/beyond/algo/common/PasswordEncrypUtil.java
@@ -25,10 +25,10 @@ public class PasswordEncrypUtil {
             SecureRandom random = new SecureRandom();
             DESKeySpec desKey = new DESKeySpec(password.getBytes());
             //创建一个密匙工厂，然后用它把DESKeySpec转换成
-            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PasswordEncrypUtil");
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
             SecretKey securekey = keyFactory.generateSecret(desKey);
             //Cipher对象实际完成加密操作
-            Cipher cipher = Cipher.getInstance("PasswordEncrypUtil");
+            Cipher cipher = Cipher.getInstance("DES");
             //用密匙初始化Cipher对象
             cipher.init(Cipher.ENCRYPT_MODE, securekey, random);
             //现在，获取数据并加密
@@ -54,11 +54,11 @@ public class PasswordEncrypUtil {
         // 创建一个DESKeySpec对象
         DESKeySpec desKey = new DESKeySpec(password.getBytes());
         // 创建一个密匙工厂
-        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PasswordEncrypUtil");
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
         // 将DESKeySpec对象转换成SecretKey对象
         SecretKey securekey = keyFactory.generateSecret(desKey);
         // Cipher对象实际完成解密操作
-        Cipher cipher = Cipher.getInstance("PasswordEncrypUtil");
+        Cipher cipher = Cipher.getInstance("DES");
         // 用密匙初始化Cipher对象
         cipher.init(Cipher.DECRYPT_MODE, securekey, random);
         // 真正开始解密操作
