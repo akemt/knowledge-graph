ac53c756f2f35888807571aad12fb6deda372ed5
matrix_qihe <qihea163@163.com>
2017-09-29 10:54:01 +0800

    总体框架修改

--- algo-console-boot/src/main/java/com/beyond/algo/algoconsoleboot/AlgoConsoleBootApplication.java
+++ algo-console-boot/src/main/java/com/beyond/algo/algoconsoleboot/AlgoConsoleBootApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.beyond.algo.controller","com.beyond.algo.infra","com.beyond.algo.model"})
+@MapperScan(basePackages = "com.beyond.algo.mapper")
 public class AlgoConsoleBootApplication {
 
 	public static void main(String[] args) {
--- algo-console-boot/src/main/java/com/beyond/algo/controller/UserController.java
+++ algo-console-boot/src/main/java/com/beyond/algo/controller/UserController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
  * @date ：13:16 2017/9/25
  */
 @RestController
-@EnableAutoConfiguration
 @RequestMapping("/user")
 public class UserController {
 
