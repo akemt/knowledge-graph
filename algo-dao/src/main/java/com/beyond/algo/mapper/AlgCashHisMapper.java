4c09b0194df1139d545a31fe1df1a74bd2dd5fea
zhang1chang0 <1625488654@qq.com>
2017-10-10 16:38:57 +0800

    增加提现表

---
+++ algo-dao/src/main/java/com/beyond/algo/mapper/AlgCashHisMapper.java
@@ -0,0 +1,16 @@
+package com.beyond.algo.mapper;
+
+import com.beyond.algo.model.AlgCashHis;
+import java.util.List;
+
+public interface AlgCashHisMapper {
+    int deleteByPrimaryKey(String traSn);
+
+    int insert(AlgCashHis record);
+
+    AlgCashHis selectByPrimaryKey(String traSn);
+
+    List<AlgCashHis> selectAll();
+
+    int updateByPrimaryKey(AlgCashHis record);
+}
