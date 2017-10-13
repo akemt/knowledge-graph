a282e30b0fd2c232464762ff115c027afd8b4d22
zhang1chang0 <1625488654@qq.com>
2017-10-13 08:57:01 +0800

    删除get,set方法改成标签@Data

--- algo-dao/src/main/java/com/beyond/algo/vo/AlgRUserModuleCallTransVo.java
+++ algo-dao/src/main/java/com/beyond/algo/vo/AlgRUserModuleCallTransVo.java
@@ -1,55 +1,19 @@
 package com.beyond.algo.vo;
 
 import com.beyond.algo.model.AlgRUserModuleCallTrans;
+import lombok.Data;
 
 /**
  * @author ：zhangchuanzhi
  * @Description:用户调用查询
  * @date ：13:04 2017/10/12
  */
+@Data
 public class AlgRUserModuleCallTransVo extends AlgRUserModuleCallTrans {
     private int page;
     private int rows;
     private int total;
     private String modName;
     private String createTime;
-    public int getPage() {
-        return page;
-    }
 
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public int getRows() {
-        return rows;
-    }
-
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
-    public String getModName() {
-        return modName;
-    }
-
-    public void setModName(String modName) {
-        this.modName = modName;
-    }
-
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
 }
--- algo-dao/src/main/java/com/beyond/algo/vo/PayRecordVo.java
+++ algo-dao/src/main/java/com/beyond/algo/vo/PayRecordVo.java
@@ -1,12 +1,14 @@
 package com.beyond.algo.vo;
 
 import com.beyond.algo.model.AlgCashTrans;
+import lombok.Data;
 
 /**
  * @author ：zhangchuanzhi
  * @Description:充值记录
  * @date ：10:43 2017/10/12
  */
+@Data
 public class PayRecordVo extends AlgCashTrans {
 
     private int page;
@@ -15,35 +17,5 @@ public class PayRecordVo extends AlgCashTrans {
     private int integral;
 
     private int total;
-    public int getPage() {
-        return page;
-    }
 
-    public void setPage(int page) {
-        this.page = page;
-    }
-
-    public int getRows() {
-        return rows;
-    }
-
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
-    public int getIntegral() {
-        return integral;
-    }
-
-    public void setIntegral(int integral) {
-        this.integral = integral;
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
 }
