5afa371875513284cbd54f41a65891b5eb383932
zhangxianjie <xj_zh@foxmail.com>
2017-10-17 13:42:17 +0800

    AuthCodeController 更新  添加Test方法 张现杰

--- algo-console-boot/src/test/java/com/beyond/algo/algoconsoleboot/AuthCodeControllerTest.java
+++ algo-console-boot/src/test/java/com/beyond/algo/algoconsoleboot/AuthCodeControllerTest.java
@@ -1,5 +1,6 @@
 package com.beyond.algo.algoconsoleboot;
 
+import com.beyond.algo.common.UUIDUtil;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,6 +24,8 @@ public class AuthCodeControllerTest {
     protected WebApplicationContext wac;
     private MockMvc mockMvc;
 
+
+    //private String acdSn; //本表主键
     @Before
     public void setup() throws Exception {
         this.mockMvc = webAppContextSetup(this.wac).alwaysExpect(status().isOk()).build();
@@ -32,12 +35,46 @@ public class AuthCodeControllerTest {
     }
     @Test
     public void create() throws Exception {
+        String acdSn = UUIDUtil.createUUID();
+        String usrSn = UUIDUtil.createUUID();
         String result = this.mockMvc.perform(post("/authcode/create").contentType(MediaType.APPLICATION_JSON)
-                .param("acdSn","aac44b648b10429cbaf85a6ae0113a85")
-                .param("usrSn","aac44b648b10430cbaf85a6ae0113a75")
+                .param("acdSn",acdSn)
+                .param("usrSn",usrSn)
                 .param("acdName","thisisacdname")
                 .param("acdId","thisisacdid001")
                 .param("callFromClient","1"))
+                .andExpect(status().is(200)).andReturn().getResponse().getContentAsString();
+        System.out.println(result);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        //通过主键删除
+        String result = this.mockMvc.perform(get("/authcode/delete/{acdSn_id}","aac44b648b10429cbaf85a6ae0113a45").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(200)).andReturn().getResponse().getContentAsString();
+        System.out.println(result);
+    }
+
+    @Test
+    public void update() throws Exception {
+        String result = this.mockMvc.perform(post("/authcode/update").contentType(MediaType.APPLICATION_JSON)
+                .param("acdSn","8303420b322d43b1bc6bdd63d2103fa3")  //从数据库里面取出id
+                .param("usrSn","8303430b322d43b1bc6bdd63d2103fa3")
+                .param("acdName","thisid")
+                .param("acdId","thisid00")
+                .param("callFromClient","1"))
+                .andExpect(status().is(200)).andReturn().getResponse().getContentAsString();
+        System.out.println(result);
+    }
+    @Test
+    public void select() throws Exception {
+        String result = this.mockMvc.perform(get("/authcode/select/{acdSn_id}","8303420b322d43b1bc6bdd63d2103fa3").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(200)).andReturn().getResponse().getContentAsString();
+        System.out.println(result);
+    }
+    @Test
+    public void list() throws Exception {
+        String result = this.mockMvc.perform(post("/authcode/list").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is(200)).andReturn().getResponse().getContentAsString();
         System.out.println(result);
     }
