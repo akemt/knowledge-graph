c4a61a18ff54d8109808cc16089e3aa9727af536
zhang1chang0 <1625488654@qq.com>
2017-10-17 10:41:12 +0800

    增加购买积分VO

---
+++ algo-dao/src/main/java/com/beyond/algo/vo/PayVo.java
@@ -0,0 +1,14 @@
+package com.beyond.algo.vo;
+
+import lombok.Data;
+
+/**
+ * @author ：zhangchuanzhi
+ * @Description:充值购买积分
+ * @date ：16:25 2017/10/16
+ */
+@Data
+public class PayVo {
+    private Float money;
+    private Float rate;
+}
