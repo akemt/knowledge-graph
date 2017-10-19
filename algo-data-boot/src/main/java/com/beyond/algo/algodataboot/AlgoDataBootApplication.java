dabe528bd2188964fa904057a170bb17fccf6f01
matrix_qihe <qihea163@163.com>
2017-10-19 18:52:25 +0800

    祁贺 修改扫描配置

--- algo-data-boot/src/main/java/com/beyond/algo/algodataboot/AlgoDataBootApplication.java
+++ algo-data-boot/src/main/java/com/beyond/algo/algodataboot/AlgoDataBootApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.beyond.algo.model"})
 @MapperScan(basePackages = "com.beyond.algo.mapper")
 public class AlgoDataBootApplication {
 
