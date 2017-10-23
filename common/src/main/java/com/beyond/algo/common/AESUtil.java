e705a7a1393623f5b670a95e64e41100dd7a0813
ZhangNYG <894573940@qq.com>
2017-10-23 13:33:36 +0800

    加密算法

--- common/src/main/java/com/beyond/algo/common/AESUtil.java
+++ common/src/main/java/com/beyond/algo/common/AESUtil.java
@@ -1,23 +1,14 @@
 package com.beyond.algo.common;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
-/**
-* @Author: JIE
-* @version：2017/10/8 0008 下午 7:58
-  * @param :null
-*/
-//程序中的代码有关转换成String的函数或部分都注释了，用不着的是，直接用base64进行 decode和encode
 public class AESUtil {
-	public static String encrypt(String dataSource, String password) {
+	public static byte[] encrypt(String content, String password) {
 		try {
 			KeyGenerator kgen = KeyGenerator.getInstance("AES");
 			kgen.init(128, new SecureRandom(password.getBytes()));
@@ -25,17 +16,17 @@ public class AESUtil {
 			byte[] enCodeFormat = secretKey.getEncoded();
 			SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
 			Cipher cipher = Cipher.getInstance("AES");// 创建密码器
-			//byte[] dataSource = dataSource.getBytes("utf-8");
+			byte[] byteContent = content.getBytes("utf-8");
 			cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
-			byte[] result = cipher.doFinal(dataSource.getBytes());
-			//将数据结果转换成字符串结果返回
-			String stringResult = new BASE64Encoder().encode(result);
-			return stringResult; // 加密
+			byte[] result = cipher.doFinal(byteContent);
+			return result; // 加密
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		} catch (NoSuchPaddingException e) {
 			e.printStackTrace();
 		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IllegalBlockSizeException e) {
 			e.printStackTrace();
@@ -44,13 +35,37 @@ public class AESUtil {
 		}
 		return null;
 	}
-	
-	/**
-	* @Author: JIE
-	* @version：2017/10/8 0008 下午 8:03
-	  * @param: null
-	*/
-	public static String decrypt(String dataSource, String password) {
+
+	public static String encryptString(String content, String password) {
+		try {
+			KeyGenerator kgen = KeyGenerator.getInstance("AES");
+			kgen.init(128, new SecureRandom(password.getBytes()));
+			SecretKey secretKey = kgen.generateKey();
+			byte[] enCodeFormat = secretKey.getEncoded();
+			SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+			Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+			byte[] byteContent = content.getBytes("utf-8");
+			cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
+			byte[] result = cipher.doFinal(byteContent);
+			String encryptResultStr = parseByte2HexStr(result);
+			return encryptResultStr;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static byte[] decrypt(byte[] content, String password) {
 		try {
 			KeyGenerator kgen = KeyGenerator.getInstance("AES");
 			kgen.init(128, new SecureRandom(password.getBytes()));
@@ -59,10 +74,8 @@ public class AESUtil {
 			SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
 			Cipher cipher = Cipher.getInstance("AES");// 创建密码器
 			cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
-			byte[] byteSource = new BASE64Decoder().decodeBuffer(dataSource);
-			byte[] result = cipher.doFinal(byteSource);
-			//将数据结果转换成字符串结果
-			return new String(result); // 加密
+			byte[] result = cipher.doFinal(content);
+			return result; // 加密
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		} catch (NoSuchPaddingException e) {
@@ -73,9 +86,75 @@ public class AESUtil {
 			e.printStackTrace();
 		} catch (BadPaddingException e) {
 			e.printStackTrace();
-		} catch (IOException e) {
+		}
+		return null;
+	}
+
+	public static String decrypt(String content, String password) {
+		try {
+			SecretKey secretKey = getKey(password);
+			byte[] enCodeFormat = secretKey.getEncoded();
+			SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			byte[] decryptFrom = parseHexStr2Byte(content);
+			byte[] result = cipher.doFinal(decryptFrom);
+			return new String(result);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			e.printStackTrace();
+		} catch (Exception e){
 			e.printStackTrace();
 		}
 		return null;
 	}
+
+	public static String parseByte2HexStr(byte buf[]) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < buf.length; i++) {
+			String hex = Integer.toHexString(buf[i] & 0xFF);
+			if (hex.length() == 1) {
+				hex = '0' + hex;
+			}
+			sb.append(hex.toUpperCase());
+		}
+		return sb.toString();
+	}
+
+	public static byte[] parseHexStr2Byte(String hexStr) {
+		if (hexStr.length() < 1)
+			return null;
+		byte[] result = new byte[hexStr.length() / 2];
+		for (int i = 0; i < hexStr.length() / 2; i++) {
+			int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
+			int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
+			result[i] = (byte) (high * 16 + low);
+		}
+		return result;
+	}
+
+	public static SecretKey getKey(String password) {
+		try {
+			KeyGenerator _generator = KeyGenerator.getInstance( "AES" );
+			SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG" );
+			secureRandom.setSeed(password.getBytes());
+			_generator.init(128,secureRandom);
+			return _generator.generateKey();
+		}  catch (Exception e) {
+			throw new RuntimeException( " 初始化密钥出现异常 " );
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(encryptString("123456","49475801beyondjvkduedfj"));
+		System.out.println(decrypt("B2AC264948ACA0906B9F12194F5564A9","49475801beyondjvkduedfj"));
+	}
+
 }
