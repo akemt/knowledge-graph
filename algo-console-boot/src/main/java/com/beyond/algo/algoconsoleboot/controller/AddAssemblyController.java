692f917d4eda40b3c0b19c0b2099788a10278d5e
zhangxianjie <xj_zh@foxmail.com>
2017-11-01 11:33:25 +0800

    张海洋 算法新增接口的初始化

--- algo-console-boot/src/main/java/com/beyond/algo/algoconsoleboot/controller/AddAssemblyController.java
+++ algo-console-boot/src/main/java/com/beyond/algo/algoconsoleboot/controller/AddAssemblyController.java
@@ -16,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author ：lindewei
@@ -78,6 +80,38 @@ public class AddAssemblyController {
             //集群
             List<AlgDic> algDic =algDicMapper.getDictionarylist(dicCode);
             return Result.ok(algDic);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result<Object>(ResultEnum.FAILURE.code, e.getMessage());
+        }
+    }
+
+    /**
+     * @author ：lindewei
+     * @Description:集群
+     * @param：dicSort 分类
+     */
+    @RequestMapping(value = "/colony", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Result init(String dicCode) {
+        try {
+            //集群
+            Map<String,List> map=new HashMap<String,List>();
+            String moduleAccessMode="module_access_mode";
+            List<AlgDic> AccessandCall =algDicMapper.getDictionarylist(moduleAccessMode);
+            map.put("moduleAccessMode",AccessandCall);
+            String runEnv="run_env";
+            List<AlgDic> RunandEnv =algDicMapper.getDictionarylist(runEnv);
+            map.put("runEnv",RunandEnv);
+            String iscolony="is_colony";
+            List<AlgDic> SingandClu =algDicMapper.getDictionarylist(dicCode);
+            map.put("iscolony",SingandClu);
+            String standenv="stand_env";
+            List<AlgDic> Single =algDicMapper.getDictionarylist(dicCode);
+            map.put("standenv",Single);
+            String gpuenv="gpu_env";
+            List<AlgDic> Cluster =algDicMapper.getDictionarylist(dicCode);
+            map.put("gpuenv",Cluster);
+            return Result.ok(map);
         } catch (Exception e) {
             e.printStackTrace();
             return new Result<Object>(ResultEnum.FAILURE.code, e.getMessage());
