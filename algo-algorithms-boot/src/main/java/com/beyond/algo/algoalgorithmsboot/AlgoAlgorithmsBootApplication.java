a71bc8234e89cadc8cc4ded1372d564d0e134fae
matrix_qihe <qihea163@163.com>
2017-11-10 11:26:05 +0800

    祁贺 修改 module add 路径

--- algo-algorithms-boot/src/main/java/com/beyond/algo/algoalgorithmsboot/AlgoAlgorithmsBootApplication.java
+++ algo-algorithms-boot/src/main/java/com/beyond/algo/algoalgorithmsboot/AlgoAlgorithmsBootApplication.java
@@ -3,12 +3,10 @@ package com.beyond.algo.algoalgorithmsboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@EnableResourceServer
 @MapperScan(basePackages = "com.beyond.algo.mapper")
 public class AlgoAlgorithmsBootApplication {
 
