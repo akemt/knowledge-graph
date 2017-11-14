b42b57b7d74096c73a0653b3531e436dbc8911d2
matrix_qihe <qihea163@163.com>
2017-11-14 09:52:29 +0800

    祁贺 修改 编译模块接口

---
+++ algo-data-boot/src/main/java/com/beyond/algo/algodataboot/config/ResourceServerConfiguration.java
@@ -0,0 +1,24 @@
+package com.beyond.algo.algodataboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * @Author: qihe
+ * @Description:
+ * @Date: create in 2017/11/10 10:45
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration  extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/register",
+                        "/module/list"
+                ).permitAll().and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+}
