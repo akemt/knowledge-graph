89f1a8e63c0739ed0676501bb4cd27a85680c1e7
matrix_qihe <qihea163@163.com>
2017-11-21 14:42:35 +0800

    修改项目名称等

--- common/src/main/java/com/beyond/algm/common/AdapterUtil.java
+++ common/src/main/java/com/beyond/algm/common/AdapterUtil.java
@@ -12,7 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 public class AdapterUtil {
     public static Object moduleAdapter(String lanName)  throws AlgException {
         try {
-            return Class.forName("com.beyond.algo.algoalgorithmsboot.adapter."+ lanName +"ModuleAdapter").newInstance();
+
+            return Class.forName("com.beyond.algm.algmalgorithmsboot.adapter."+ lanName +"ModuleAdapter").newInstance();
         } catch (Exception e) {
             log.error(e.getMessage());
             throw new AlgException("BEYOND.ALG.MODULE.COPY.0000006",new String[]{},e);
