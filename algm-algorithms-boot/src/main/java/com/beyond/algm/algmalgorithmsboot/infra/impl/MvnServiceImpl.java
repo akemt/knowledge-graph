117bf33c9527cbc51be5f81ee60fc7d97e7bf7b1
matrix_qihe <qihea163@163.com>
2017-11-23 12:39:34 +0800

    修改maven api 实现

--- algm-algorithms-boot/src/main/java/com/beyond/algm/algmalgorithmsboot/infra/impl/MvnServiceImpl.java
+++ algm-algorithms-boot/src/main/java/com/beyond/algm/algmalgorithmsboot/infra/impl/MvnServiceImpl.java
@@ -18,21 +18,27 @@ import java.util.Arrays;
 @Service
 @Slf4j
 public class MvnServiceImpl implements MvnService {
+    private static String mavenHomePath;
+    static {
+        mavenHomePath = System.getenv("MAVEN_HOME");
+        if(Assert.isEmpty(mavenHomePath) || "".equals(mavenHomePath)){
+            mavenHomePath = System.getenv("M2_HOME");
+        }
+        if(Assert.isEmpty(mavenHomePath) || "".equals(mavenHomePath)){
+            log.error("Can not get maven env,please install apache maven and configuring environment variables");
+        }
+    }
 
     public void mvnPackageMod() throws AlgException{
+        if(Assert.isEmpty(mavenHomePath) || "".equals(mavenHomePath)){
+            throw new AlgException("发布项目失败，获取不到 maven 路径！");
+        }
         InvocationRequest request = new DefaultInvocationRequest();
         request.setPomFile( new File( "D:/IdeaProjects/algmarket-publish/pom.xml" ) );
         request.setGoals( Arrays.asList( "clean", "package -Dmaven.test.skip=true"));
 
         Invoker invoker = new DefaultInvoker();
-        String mavenHomePath = System.getenv("MAVEN_HOME");
-        if(Assert.isEmpty(mavenHomePath)|| "".equals(mavenHomePath)){
-            mavenHomePath = System.getenv("M2_HOME");
-        }
-        if(Assert.isEmpty(mavenHomePath)|| "".equals(mavenHomePath)){
-            log.error("Can not get maven env,please install apache maven and configuring environment variables");
-            throw new AlgException("发布项目失败，获取不到 maven 路径！");
-        }
+
         invoker.setMavenHome(new File(mavenHomePath));
         try {
             invoker.execute(request);
