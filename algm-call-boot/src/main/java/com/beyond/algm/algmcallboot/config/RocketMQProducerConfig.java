ce51a4526000935a77be19c07bd5766d08f6ab7b
ahtZzz-xiao <848141593@qq.com>
2017-11-24 18:11:07 +0800

    rocketmq模块功能追加。

---
+++ algm-call-boot/src/main/java/com/beyond/algm/algmcallboot/config/RocketMQProducerConfig.java
@@ -0,0 +1,54 @@
+package com.beyond.algm.algmcallboot.config;
+
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.beyond.algm.common.Assert;
+import com.beyond.algm.exception.AlgException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+@Slf4j
+public class RocketMQProducerConfig {
+    @Value("${rocketmq.producer.groupName}")
+    private String groupName;
+    @Value("${rocketmq.producer.namesrvAddr}")
+    private String namesrvAddr;
+    @Value("${rocketmq.producer.instanceName}")
+    private String instanceName;
+    @Value("${rocketmq.producer.maxMessageSize}")
+    private int maxMessageSize ;
+    @Value("${rocketmq.producer.sendMsgTimeout}")
+    private int sendMsgTimeout ;
+
+    @Bean
+    public DefaultMQProducer getRocketMQProducer() throws AlgException {
+        if (Assert.isEmpty(this.groupName)) {
+            throw new AlgException("groupName is blank");
+        }
+        if (Assert.isEmpty(this.namesrvAddr)) {
+            throw new AlgException("nameServerAddr is blank");
+        }
+        if (Assert.isEmpty(this.instanceName)){
+            throw new AlgException("instanceName is blank");
+        }
+        DefaultMQProducer producer;
+        producer = new DefaultMQProducer(this.groupName);
+        producer.setNamesrvAddr(this.namesrvAddr);
+        producer.setInstanceName(instanceName);
+        producer.setMaxMessageSize(this.maxMessageSize);
+        producer.setSendMsgTimeout(this.sendMsgTimeout);
+        try {
+            producer.start();
+            log.info(String.format("producer is start ! groupName:[%s],namesrvAddr:[%s]"
+                    , this.groupName, this.namesrvAddr));
+        } catch (MQClientException e) {
+            log.error(String.format("producer is error {}"
+                    , e.getMessage(),e));
+            throw new AlgException(e);
+        }
+        return producer;
+    }
+}
---
+++ algm-call-boot/src/test/java/com/beyond/algm/algmcallboot/infra/impl/AlgUserCallServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.beyond.algm.algmcallboot.infra.impl;
+
+import com.beyond.algm.algmcallboot.AlgmCallBootApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = AlgmCallBootApplication.class)
+public class AlgUserCallServiceImplTest {
+    @Test
+    public void findByCallUsrSn() throws Exception {
+
+    }
+
+}
