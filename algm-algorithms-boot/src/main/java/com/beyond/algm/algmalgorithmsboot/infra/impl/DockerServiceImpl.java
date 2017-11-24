869252010558464ed73a2da2eaaba92c08b2b68b
matrix_qihe <qihea163@163.com>
2017-11-24 17:51:02 +0800

    删除 blog 模块

--- algm-algorithms-boot/src/main/java/com/beyond/algm/algmalgorithmsboot/infra/impl/DockerServiceImpl.java
+++ algm-algorithms-boot/src/main/java/com/beyond/algm/algmalgorithmsboot/infra/impl/DockerServiceImpl.java
@@ -2,8 +2,15 @@ package com.beyond.algm.algmalgorithmsboot.infra.impl;
 
 import com.beyond.algm.algmalgorithmsboot.infra.DockerService;
 import com.beyond.algm.exception.AlgException;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.command.BuildImageResultCallback;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 /**
  * @Author: qihe
@@ -15,6 +22,21 @@ import org.springframework.stereotype.Service;
 public class DockerServiceImpl implements DockerService {
 
     public void makeDockerImage() throws AlgException{
-
+        DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                //主机URL，例如tcp://localhost:2376或unix:///var/run/docker.sock
+                .withDockerHost("tcp://192.168.1.94:2375")
+                //启用/禁用TLS验证（切换http和https协议）
+                .withDockerTlsVerify(false)
+                //.withDockerCertPath("/home/user/.docker/certs")
+                //额外的docker配置文件路径
+                //.withDockerConfig("/home/user/.docker")
+                .withApiVersion("1.23")
+                /*.withRegistryUrl("http://192.168.1.94:2375/v1/")
+                .withRegistryUsername("admin")
+                .withRegistryPassword("Harbor12345")
+                .withRegistryEmail("admin@example.com")*/
+                .build();
+        dockerClient.buildImageCmd(new File("E:/dockerfile", "Dockerfile")).withNoCache(true).exec(new BuildImageResultCallback()).awaitImageId();
     }
 }
