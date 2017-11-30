a28953efb2807ccb6517a26481813b8b17381e66
matrix_qihe <qihea163@163.com>
2017-11-30 14:44:08 +0800

    修改 公共路径方法

--- algm-algorithms-boot/src/main/java/com/beyond/algm/algmalgorithmsboot/infra/impl/PathServiceImpl.java
+++ algm-algorithms-boot/src/main/java/com/beyond/algm/algmalgorithmsboot/infra/impl/PathServiceImpl.java
@@ -4,7 +4,6 @@ import com.beyond.algm.algmalgorithmsboot.infra.PathService;
 import com.beyond.algm.algmalgorithmsboot.model.GitConfigModel;
 import com.beyond.algm.algmalgorithmsboot.model.ProjectConfigModel;
 import com.beyond.algm.exception.AlgException;
-import com.beyond.algm.model.AlgProgramLang;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
