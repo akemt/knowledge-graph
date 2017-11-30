b92c66f764a8e7c4ec35aaa43517543d83fd8099
matrix_qihe <qihea163@163.com>
2017-11-30 15:14:47 +0800

    修改 公共路径方法

--- algm-algorithms-boot/src/main/java/com/beyond/algm/algmalgorithmsboot/infra/impl/DataAndModelListServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.beyond.algm.algmalgorithmsboot.infra.impl;
-
-/**
- * @author ：zhangchuanzhi
- * @Description:数据和模型数据列表
- * @date ：10:45 2017/10/17
- */
-public class DataAndModelListServiceImpl {
-}
