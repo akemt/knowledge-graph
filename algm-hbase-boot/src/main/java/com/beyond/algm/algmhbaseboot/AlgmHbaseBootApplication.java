887bcc8ffaf8b872fe46ae9c587176df518f06ac
matrix_qihe <qihea163@163.com>
2017-12-08 17:17:26 +0800

    创建hbase模块

---
+++ algm-hbase-boot/src/main/java/com/beyond/algm/algmhbaseboot/AlgmHbaseBootApplication.java
@@ -0,0 +1,12 @@
+package com.beyond.algm.algmhbaseboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgmHbaseBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgmHbaseBootApplication.class, args);
+	}
+}
