dbb62fa33ffe97ddfa5516c404e2610bb8a8ad82
ahtZzz-xiao <848141593@qq.com>
2017-12-12 16:26:48 +0800

    分页修改：algorithms/authCode/listAuthCode

--- algm-algorithms-boot/src/main/java/com/beyond/algm/algmalgorithmsboot/infra/AuthCodeService.java
+++ algm-algorithms-boot/src/main/java/com/beyond/algm/algmalgorithmsboot/infra/AuthCodeService.java
@@ -1,9 +1,8 @@
 package com.beyond.algm.algmalgorithmsboot.infra;
 
 import com.beyond.algm.model.AlgAuthCode;
-
-import java.util.List;
-
+import com.github.pagehelper.PageInfo;
+import org.springframework.data.domain.Pageable;
 /**
  * @author XianjieZhang E-mail:xj_zh@foxmail.com
  * @version Created in：2017/10/12 0012 下午 9:48
@@ -12,5 +11,5 @@ public interface AuthCodeService {
     void generateKey(AlgAuthCode algAuthCode,String[] addUrl);
     void deleteAuthCode(String acdSn);
     void updateAuthCode(AlgAuthCode algAuthCode,String[] addUrl);
-    List<AlgAuthCode> listUserAuthCode(String usrSn);
+    PageInfo<AlgAuthCode> listUserAuthCode(String usrSn, Pageable pageable);
 }
