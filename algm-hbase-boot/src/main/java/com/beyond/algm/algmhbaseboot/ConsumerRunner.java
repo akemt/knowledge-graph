da14e351f55e0e69bff75e3146fa61d8f9fe5d93
zhangxianjie <xj_zh@foxmail.com>
2017-12-19 16:03:24 +0800

    test

--- algm-hbase-boot/src/main/java/com/beyond/algm/algmhbaseboot/ConsumerRunner.java
+++ algm-hbase-boot/src/main/java/com/beyond/algm/algmhbaseboot/ConsumerRunner.java
@@ -2,6 +2,7 @@ package com.beyond.algm.algmhbaseboot;
 
 import com.alibaba.fastjson.JSONObject;
 import com.beyond.algm.algmhbaseboot.infra.HBaseService;
+import com.beyond.algm.algmhbaseboot.infra.PhoenixRunner;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
@@ -18,6 +19,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringBootConfiguration;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -40,6 +42,8 @@ public class ConsumerRunner implements ApplicationRunner {
     private String nameSrvAddr;
     @Value("${rocketMQ.topic1}")
     private String rocketMQTopic1;
+    @Value("${rocketMQ.topic2}")
+    private String rocketMQTopic2;
     @Value("${hbase.tableName1}")
     private String tableName1;
 
@@ -47,13 +51,16 @@ public class ConsumerRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments var1) throws Exception{
 
+        // phoenix对象
+        PhoenixRunner phoenixRunner = new PhoenixRunner();
+
         // create PushConsumer instance
         DefaultMQPushConsumer pushConsumer = new DefaultMQPushConsumer(rocketMQGroup1);
         // Specify which nameServer to connect
         pushConsumer.setNamesrvAddr(nameSrvAddr);
         // Specify the topic of the subscription
         try {
-            pushConsumer.subscribe(rocketMQTopic1, "*");
+            pushConsumer.subscribe(rocketMQTopic2, "*");
         } catch (MQClientException e) {
             e.printStackTrace();
         }
@@ -81,10 +88,17 @@ public class ConsumerRunner implements ApplicationRunner {
                     log.info(jsonValue.toString());
                     //-----------------------------------------------
                     //------------------有问题，少条，少字段（同时大批量）；无问题（非同时）
-
+                    /* 直接写入hbase
                     try {
                         hBaseService.insertTableByJson(tableName1, jsonValue);
                     } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    */
+                    // 使用phoenix写入hbase
+                    try {
+                        phoenixRunner.insertByJson(jsonValue);
+                    } catch (SQLException e) {
                         e.printStackTrace();
                     }
                 }
--- algm-hbase-boot/src/main/java/com/beyond/algm/algmhbaseboot/infra/PhoenixRunner.java
+++ algm-hbase-boot/src/main/java/com/beyond/algm/algmhbaseboot/infra/PhoenixRunner.java
@@ -1,5 +1,6 @@
 package com.beyond.algm.algmhbaseboot.infra;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,6 +20,8 @@ public class PhoenixRunner {
 
     @Value("${phoenix.zkHosts}")
     private String zkHosts;
+    @Value("${phoenix.className}")
+    private String className;
 
     private Connection connection;
     private Statement statement;
@@ -29,7 +32,7 @@ public class PhoenixRunner {
     public PhoenixRunner(){
         // phoenix驱动
         try {
-            Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
+            Class.forName(className);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -67,9 +70,7 @@ public class PhoenixRunner {
         String owner_usr_sn = "-1";
         // 各种使用信息
         long duration = 0;
-        int billed_call_cnt = 0;
-        int call_pay_amount = 0;
-        int mod_call_cnt = 0;
+        float call_pay_amount = 0;
         int is_charging = 0;
         long start_time = 0;
         long end_time = 0;
@@ -80,43 +81,46 @@ public class PhoenixRunner {
             switch(key)
             {
                 // 各种串号
-                case "alg_sn":
+                case "algSn":
                     alg_sn = obj.getString(key);
                     break;
-                case "umc_sn":
+                case "umcSn":
                     umc_sn = obj.getString(key);
                     break;
-                case "mod_sn":
+                case "modSn":
                     mod_sn = obj.getString(key);
                     break;
-                case "ver_sn":
+                case "verSn":
                     ver_sn = obj.getString(key);
                     break;
-                case "call_usr_sn":
+                case "callUsrSn":
                     call_usr_sn = obj.getString(key);
                     break;
-                case "owner_usr_sn":
+                case "ownerUsrSn":
                     owner_usr_sn = obj.getString(key);
                     break;
                 // 各种使用信息
                 case "duration":
                     duration = Long.parseLong(obj.getString(key));
-                case "billed_call_cnt":
-                    billed_call_cnt = Integer.parseInt(obj.getString(key));
-                case "call_pay_amount":
-                    call_pay_amount = Integer.parseInt(obj.getString(key));
-                case "mod_call_cnt":
-                    mod_call_cnt = Integer.parseInt(obj.getString(key));
-                case "is_charging":
+                    break;
+                case "callPayAmount":
+                    call_pay_amount = Float.parseFloat(obj.getString(key));
+                    break;
+                case "isCharging":
                     is_charging = Integer.parseInt(obj.getString(key));
-                case "start_time":
+                    break;
+                case "startTime":
                     start_time = Long.parseLong(obj.getString(key));
-                case "end_time":
+                    break;
+                case "endTime":
                     end_time = Long.parseLong(obj.getString(key));
+                    break;
             }
         }
         // sql语句
-        String sql = "UPSERT INTO MYTEST(ALGSN,TEST1,TEST2,TEST3,TEST4) VALUES ("
+        String sql = "UPSERT INTO ALG_R_USER_MODULE_CALL_TRANS("
+                + "ALG_SN,UMC_SN,MOD_SN,VER_SN,CALL_USR_SN,OWNER_USR_SN,DURATION,CALL_PAY_AMOUNT,IS_CHARGING,START_TIME,END_TIME"
+                + ") VALUES ("
                 + "'" + alg_sn + "'" + ","
                 + "'" + umc_sn + "'" + ","
                 + "'" + mod_sn + "'" + ","
@@ -124,16 +128,125 @@ public class PhoenixRunner {
                 + "'" + call_usr_sn + "'" + ","
                 + "'" + owner_usr_sn + "'" + ","
                 + duration + ","
-                + billed_call_cnt + ","
                 + call_pay_amount + ","
-                + mod_call_cnt + ","
                 + is_charging + ","
                 + start_time + ","
-                + end_time + ","
+                + end_time + ")"
                 ;
+        // 提交sql语句
+        statement.executeUpdate(sql);
+        connection.commit();
     }
+    /**
+     * query
+     * */
+    public void query () {
+        // 获取系统时间(缓冲时间10分钟)
+        long endTimeStamp = System.currentTimeMillis()-600000;
+        // 5分钟
+        long startTimeStamp = endTimeStamp-300000;
+        // 指定sql语句
+        String sql = "SELECT " +
+                "TA.UMC_SN as UMC_SN, " +
+                "TA.MOD_SN as MOD_SN, " +
+                "TA.VER_SN as VER_SN, " +
+                "TA.CALL_USR_SN as CALL_USR_SN, " +
+                "TA.OWNER_USR_SN as OWNER_USR_SN, " +
+                "TA.SUM_DURATION as SUM_DURATION, " +
+                "TA.SUM_CALL_PAY_AMOUNT as SUM_CALL_PAY_AMOUNT, " +
+                "TA.SUM_ALL_CALL_AMOUNT as SUM_ALL_CALL_AMOUNT, " +
+                "TB.SUM_BILLED_CALL_CNT as SUM_BILLED_CALL_CNT " +
+                "FROM ( " +
+                "  SELECT " +
+                "  A.UMC_SN, " +
+                "  A.MOD_SN, " +
+                "  A.VER_SN, " +
+                "  A.CALL_USR_SN, " +
+                "  A.OWNER_USR_SN, " +
+                "  B.SUM_DURATION, " +
+                "  ROUND(B.SUM_CALL_PAY_AMOUNT,2) AS SUM_CALL_PAY_AMOUNT, " +
+                "  B.SUM_ALL_CALL_AMOUNT " +
+                "  FROM ( " +
+                "    SELECT " +
+                "    DISTINCT UMC_SN,MOD_SN,VER_SN,CALL_USR_SN,OWNER_USR_SN " +
+                "    FROM ALG_R_USER_MODULE_CALL_TRANS " +
+                "    WHERE TIMEST > " + startTimeStamp +
+                "    AND TIMEST < " + endTimeStamp +
+                "  ) A LEFT JOIN ( " +
+                "    SELECT " +
+                "    UMC_SN, " +
+                "    MOD_SN, " +
+                "    VER_SN, " +
+                "    SUM(DURATION) AS SUM_DURATION, " +
+                "    SUM(CALL_PAY_AMOUNT) AS SUM_CALL_PAY_AMOUNT, " +
+                "    COUNT(1) AS SUM_ALL_CALL_AMOUNT " +
+                "    FROM ALG_R_USER_MODULE_CALL_TRANS " +
+                "    WHERE TIMEST > " + startTimeStamp +
+                "    AND TIMEST < " + endTimeStamp +
+                "    GROUP BY " +
+                "    UMC_SN, " +
+                "    MOD_SN, " +
+                "    VER_SN " +
+                "  ) B ON A.UMC_SN = B.UMC_SN " +
+                "  AND A.MOD_SN = B.MOD_SN " +
+                "  AND A.VER_SN = B.VER_SN " +
+                ") TA LEFT JOIN ( " +
+                "  SELECT  " +
+                "    UMC_SN, " +
+                "    MOD_SN, " +
+                "    VER_SN, " +
+                "    COUNT(1) AS SUM_BILLED_CALL_CNT " +
+                "  FROM ALG_R_USER_MODULE_CALL_TRANS " +
+                "  WHERE IS_CHARGING = 1 " +
+                "  AND TIMEST > " + startTimeStamp +
+                "  AND TIMEST < " + endTimeStamp +
+                "  GROUP BY " +
+                "    UMC_SN, " +
+                "    MOD_SN, " +
+                "    VER_SN " +
+                ") TB ON TA.UMC_SN = TB.UMC_SN " +
+                "AND TA.MOD_SN = TB.MOD_SN " +
+                "AND TA.VER_SN = TB.VER_SN "
+                ;
 
-    public void query (){
+        // 获取结果集 -------------------------
+        ResultSet resultSet = null;
+        try {
+            resultSet = statement.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // 将数据写入json array ----------------------
+        JSONArray jsonArray = new JSONArray();
+        JSONObject jsonObject = new JSONObject();
+
+        // 将结果遍历写入json --------------------
+        try {
+            while (resultSet.next()) {
+                // 生成json串
+                for (int i = 0; i <= resultSet.getMetaData().getColumnCount(); i++) {
+                    jsonObject.put(resultSet.getMetaData().getColumnName(i), resultSet.getString(i));
+                }
+
+                // 将生成的json串添加到json array
+                jsonArray.add(jsonObject);
+
+                // 调用mysql接口，将jsonArray写出
+//----------------这里写调用语句----------------
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
